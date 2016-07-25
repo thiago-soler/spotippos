f18a5b7ae1b9793ab3c5dbb39c181a165ee809bd
Thiago Soler <tsoler@uolinc.com>
2016-07-25 12:21:22 -0300

    Inicializando projeto.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spotipposApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spotipposApp
+ */
+angular.module('spotipposApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
